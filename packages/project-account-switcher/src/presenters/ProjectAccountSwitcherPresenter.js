d268a7472ff8a746b7063a77c8586214f8b65266
Alex Millatmal <alexandrajmh@gmail.com>
2018-07-30 11:41:38 -0400

    feat: render markup for accounts and projects programmatically from props

--- packages/project-account-switcher/src/presenters/ProjectAccountSwitcherPresenter.js
+++ packages/project-account-switcher/src/presenters/ProjectAccountSwitcherPresenter.js
@@ -11,19 +11,73 @@ import "./ProjectAccountSwitcherPresenter.scss";
 export default class ProjectAccountSwitcherPresenter extends Component {
   static propTypes = {
     activeLabel: PropTypes.string,
-    // /** Heading title for the list of Accounts */
+    /** Heading title for the list of Accounts */
     accountTitle: PropTypes.string,
-    // /** List of Accounts */
-    // accounts: PropTypes.arrayOf(),
-    // /** Menu heading title for the list of Projects */
-    projectTitle: PropTypes.string
+    /** List of Accounts */
+    accounts: PropTypes.arrayOf(
+      PropTypes.shape({
+        label: PropTypes.string
+      })
+    ),
+    /** Menu heading title for the list of Projects */
+    projectTitle: PropTypes.string,
+    /** List of Projects */
+    projects: PropTypes.arrayOf(
+      PropTypes.shape({
+        label: PropTypes.string
+      })
+    )
   };
 
   static defaultProps = {
     activeLabel: "I am a label",
     accountTitle: "Accounts",
-    projectTitle: "Projects"
+    accounts: [{ label: "Account 1" }, { label: "Account 2" }],
+    projectTitle: "Projects",
+    projects: [{ label: "Project 1" }, { label: "Project 2" }]
   };
+
+  constructPlaceholder(label) {
+    return label.match(/\b(\w)/g).join("");
+  }
+
+  accountsList() {
+    return this.props.accounts.map(account => (
+      <div className="hig__global-nav__top-nav__project-account-switcher__item hig__global-nav__top-nav__project-account-switcher__item--account hig__global-nav__top-nav__project-account-switcher__item--active">
+        <span className="hig__global-nav__top-nav__project-account-switcher__item__image-wrapper">
+          <span
+            className="hig__global-nav__top-nav__project-account-switcher__item__image-placeholder"
+            dangerouslySetInnerHTML={{
+              __html: this.constructPlaceholder(account.label)
+            }}
+          />
+        </span>
+        <span
+          className="hig__global-nav__top-nav__project-account-switcher__item__label"
+          dangerouslySetInnerHTML={{ __html: account.label }}
+        />
+      </div>
+    ));
+  }
+
+  projectsList() {
+    return this.props.projects.map(project => (
+      <div className="hig__global-nav__top-nav__project-account-switcher__item hig__global-nav__top-nav__project-account-switcher__item--project hig__global-nav__top-nav__project-account-switcher__item--active">
+        <span className="hig__global-nav__top-nav__project-account-switcher__item__image-wrapper">
+          <span
+            className="hig__global-nav__top-nav__project-account-switcher__item__image-placeholder"
+            dangerouslySetInnerHTML={{
+              __html: this.constructPlaceholder(project.label)
+            }}
+          />
+        </span>
+        <span
+          className="hig__global-nav__top-nav__project-account-switcher__item__label"
+          dangerouslySetInnerHTML={{ __html: project.label }}
+        />
+      </div>
+    ));
+  }
 
   flyoutContent() {
     return (
@@ -33,39 +87,21 @@ export default class ProjectAccountSwitcherPresenter extends Component {
             className="hig__global-nav__top-nav__project-account-switcher__list__title"
             dangerouslySetInnerHTML={{ __html: this.props.accountTitle }}
           />
-          <div className="hig__global-nav__top-nav__project-account-switcher__item hig__global-nav__top-nav__project-account-switcher__item--account hig__global-nav__top-nav__project-account-switcher__item--active">
-            <span className="hig__global-nav__top-nav__project-account-switcher__item__image-wrapper">
-              <span
-                className="hig__global-nav__top-nav__project-account-switcher__item__image-placeholder"
-                dangerouslySetInnerHTML={{ __html: "A1" }}
-              />
-            </span>
-            <span
-              className="hig__global-nav__top-nav__project-account-switcher__item__label"
-              dangerouslySetInnerHTML={{ __html: "Account 1" }}
-            />
-          </div>
+          {this.accountsList()}
         </div>
         <div className="hig__global-nav__top-nav__project-account-switcher__list">
           <div
             className="hig__global-nav__top-nav__project-account-switcher__list__title"
             dangerouslySetInnerHTML={{ __html: this.props.projectTitle }}
           />
-          <div className="hig__global-nav__top-nav__project-account-switcher__item hig__global-nav__top-nav__project-account-switcher__item--project hig__global-nav__top-nav__project-account-switcher__item--active">
-            <span className="hig__global-nav__top-nav__project-account-switcher__item__image-wrapper">
-              <span
-                className="hig__global-nav__top-nav__project-account-switcher__item__image-placeholder"
-                dangerouslySetInnerHTML={{ __html: "P1" }}
-              />
-            </span>
-            <span
-              className="hig__global-nav__top-nav__project-account-switcher__item__label"
-              dangerouslySetInnerHTML={{ __html: "Project 1" }}
-            />
-          </div>
+          {this.projectsList()}
         </div>
       </div>
     );
+  }
+
+  constructLabel() {
+    return "Account 1 / Project 1 ";
   }
 
   render() {
@@ -83,9 +119,10 @@ export default class ProjectAccountSwitcherPresenter extends Component {
                 dangerouslySetInnerHTML={{ __html: "A1/P1" }}
               />
             </span>
-            <span className="hig__global-nav__top-nav__project-account-switcher__item__label">
-              Account 1 / Project 1
-            </span>
+            <span
+              className="hig__global-nav__top-nav__project-account-switcher__item__label"
+              dangerouslySetInnerHTML={{ __html: this.constructLabel() }}
+            />
           </div>
           <span className="hig__global-nav__top-nav__project-account-switcher__target__caret">
             <div
